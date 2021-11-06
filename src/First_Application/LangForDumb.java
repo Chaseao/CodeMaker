f7683ff1199a4230325bd7b82cd8c5aaebd6031d
Ben-Clark2020 <80930714+Ben-Clark2020@users.noreply.github.com>
2021-11-06 15:47:01 -0400

    simple

--- src/First_Application/LangForDumb.java
+++ src/First_Application/LangForDumb.java
@@ -6,6 +6,11 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
+import components.simplereader.SimpleReader;
+import components.simplereader.SimpleReader1L;
+import components.simplewriter.SimpleWriter;
+import components.simplewriter.SimpleWriter1L;
+
 /**
  * @author chase
  *
@@ -15,25 +20,22 @@ public final class LangForDumb {
      * @param args
      */
     public static void main(String[] args) {
-
+        SimpleReader in = new SimpleReader1L();
+        SimpleWriter out = new SimpleWriter1L();
         JFrame frame = new JFrame("Hello World APP");
 
         frame.setMinimumSize(new Dimension(800, 600));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JLabel lblText = new JLabel("Hello World!", SwingConstants.CENTER);
-
         frame.getContentPane().add(lblText);
-
         frame.pack();
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
-
         IAlphabet currentAlphabet = null;
 
-        currentAlphabet = new English();
-
-        currentAlphabet = new Japanese();
+        out.println("What Language do you wish to learn?: ");
+        String answer = in.nextLine();
 
     }
 
