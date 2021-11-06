7971554e1b294f5836314f9511b8e731c1c39f91
Ben-Clark2020 <80930714+Ben-Clark2020@users.noreply.github.com>
2021-11-06 14:14:09 -0400

    Stuffhappened

--- src/Main/ProgramWithIOAndStaticMethod.java
+++ src/Main/ProgramWithIOAndStaticMethod.java
@@ -1,4 +1,14 @@
 package Main;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
 import components.simplereader.SimpleReader;
 import components.simplereader.SimpleReader1L;
 import components.simplewriter.SimpleWriter;
@@ -19,12 +29,28 @@ public final class ProgramWithIOAndStaticMethod {
     }
 
     /**
-     * Put a short phrase describing the static method myMethod here.
+     * reads in user input
      */
-    private static void myMethod() {
-        /*
-         * Put your code for myMethod here
-         */
+    private static String userinput(SimpleWriter out, SimpleReader in) {
+        out.println("something");
+        String datadoc = in.nextLine();
+        return datadoc;
+    }
+
+    /*
+     * Does the data reading
+     */
+    private static void dataReader(String datadoc, SimpleWriter out)
+            throws EncryptedDocumentException, IOException {
+        FileInputStream ip = new FileInputStream(datadoc);
+        Workbook wb = WorkbookFactory.create(ip);
+        Sheet sheet = wb.getSheet("Prescription_Data");
+        out.println("here");
+        for (int i = 0; i < sheet.getLastRowNum(); i++) {
+            Cell a = sheet.getRow(9).getCell(4);
+            out.println(a);
+        }
+
     }
 
     /**
@@ -32,17 +58,16 @@ public final class ProgramWithIOAndStaticMethod {
      *
      * @param args
      *            the command line arguments
+     * @throws IOException
+     * @throws EncryptedDocumentException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+            throws EncryptedDocumentException, IOException {
         SimpleReader in = new SimpleReader1L();
         SimpleWriter out = new SimpleWriter1L();
-        /*
-         * Put your main program code here; it may call myMethod as shown
-         */
-        myMethod();
-        /*
-         * Close input and output streams
-         */
+
+        dataReader(userinput(out, in), out);
+
         in.close();
         out.close();
     }
