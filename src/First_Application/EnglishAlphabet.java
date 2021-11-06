d7ba7fc6fd63c086eb0f9e008f767542781734c3
Chaseao <chaseao.oberg@gmail.com>
2021-11-06 15:56:13 -0400

    Update EnglishAlphabet.java

--- src/First_Application/EnglishAlphabet.java
+++ src/First_Application/EnglishAlphabet.java
@@ -11,9 +11,26 @@ public class EnglishAlphabet implements IAlphabet {
     /**
      *
      */
+    private Queue<Character> myAlphabet;
+
+    /**
+     * Creation Class
+     */
+    public EnglishAlphabet() {
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        for (int letterIndex = 0; letterIndex < alphabet
+                .length(); letterIndex++) {
+            this.myAlphabet.enqueue(alphabet.charAt(letterIndex));
+        }
+    }
+
+    /**
+     *
+     */
     @Override
     public Queue<Character> getAlphabet() {
         // TODO Auto-generated method stub
-        return null;
+        return this.myAlphabet;
     }
 }
