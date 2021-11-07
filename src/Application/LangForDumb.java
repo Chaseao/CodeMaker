f5c319a635dd5495583096ec5b777c06d4466508
Ben-Clark2020 <80930714+Ben-Clark2020@users.noreply.github.com>
2021-11-06 23:11:48 -0400

    Update LangForDumb.java

--- src/Application/LangForDumb.java
+++ src/Application/LangForDumb.java
@@ -18,80 +18,77 @@ public final class LangForDumb {
      * @param args
      */
     public static void main(String[] args) {
-        SimpleReader in = new SimpleReader1L();
-        SimpleWriter out = new SimpleWriter1L();
 
         final DisplayGUI display = new DisplayGUI();
+
+    }
+
+    private static void buttonDoTranslation(DisplayGUI display) {
+        SimpleReader in = new SimpleReader1L();
+        SimpleWriter out = new SimpleWriter1L();
 
         IAlphabet currentAlphabet = null;
         IAlphabet translateAlphabet = null;
 
-        out.println("What Language do you wish to translate from?: ");
         String answer = in.nextLine();
-        out.println("What Language do you wish to translate to?: ");
         String answertranslate = in.nextLine();
         int count = 0;
-
-        while ((!answer.equals("")) && !answertranslate.equals("")) {
-            answer = answer.toLowerCase();
-            answertranslate = answertranslate.toLowerCase();
-            switch (answer) {
-                case "english":
-                    currentAlphabet = new EnglishAlphabet();
-                    break;
-                case "morseCode":
-                    currentAlphabet = new MorseCodeAlphabet();
-                    break;
-                case "military":
-                    currentAlphabet = new MilitaryAlphabet();
-                    break;
-                default:
-                    count = 1;
-                    break;
-
-            }
-            switch (answertranslate) {
-                case "english":
-                    translateAlphabet = new EnglishAlphabet();
-                    break;
-                case "morsecode":
-                    translateAlphabet = new MorseCodeAlphabet();
-                    break;
-                case "military":
-                    translateAlphabet = new MilitaryAlphabet();
-                    break;
-                default:
-                    count = 1;
-                    break;
-
-            }
-            if (count == 0) {
-
-                out.println("Please enter in a String: ");
-                String userinput = in.nextLine();
-                String title = currentAlphabet.getTitle();
-                String titletranslate = translateAlphabet.getTitle();
-                components.queue.Queue<Integer> numlist = currentAlphabet
-                        .cipher(userinput);
-                if (numlist == null) {
-                    out.println("Not a valid String");
-                }
-                String translatelist = translateAlphabet.decipher(numlist);
-                display.UpdateTitleText(
-                        "Translating from " + title + " to " + titletranslate);
-                out.println("");
-                display.UpdateDecryptionText(
-                        userinput + " translates to " + translatelist);
-                out.println("");
-                out.println("What Language do you wish to translate?: ");
-                answer = in.nextLine();
-                out.println("What Language do you wish to translate to?: ");
-                answertranslate = in.nextLine();
-            } else {
-                out.println("Good Bye!");
+        answer = answer.toLowerCase();
+        answertranslate = answertranslate.toLowerCase();
+        switch (answer) {
+            case "english":
+                currentAlphabet = new EnglishAlphabet();
                 break;
-            }
+            case "morseCode":
+                currentAlphabet = new MorseCodeAlphabet();
+                break;
+            case "military":
+                currentAlphabet = new MilitaryAlphabet();
+                break;
+            default:
+                count = 1;
+                break;
 
+        }
+        switch (answertranslate) {
+            case "english":
+                translateAlphabet = new EnglishAlphabet();
+                break;
+            case "morsecode":
+                translateAlphabet = new MorseCodeAlphabet();
+                break;
+            case "military":
+                translateAlphabet = new MilitaryAlphabet();
+                break;
+            default:
+                count = 1;
+                break;
+
+        }
+        if (count == 0) {
+
+            out.println("Please enter in a String: ");
+            String userinput = in.nextLine();
+            String title = currentAlphabet.getTitle();
+            String titletranslate = translateAlphabet.getTitle();
+            components.queue.Queue<Integer> numlist = currentAlphabet
+                    .cipher(userinput);
+            if (numlist == null) {
+                out.println("Not a valid String");
+            }
+            String translatelist = translateAlphabet.decipher(numlist);
+            display.UpdateTitleText(
+                    "Translating from " + title + " to " + titletranslate);
+            out.println("");
+            display.UpdateDecryptionText(
+                    userinput + " translates to " + translatelist);
+            out.println("");
+            out.println("What Language do you wish to translate?: ");
+            answer = in.nextLine();
+            out.println("What Language do you wish to translate to?: ");
+            answertranslate = in.nextLine();
+        } else {
+            out.println("Good Bye!");
         }
 
     }
